70e1d4f608918426f272bf28e5307dd7ec67ebc6
RJZCaceres <rafaeljr.caceres@unc.edu.ph>
2019-11-08 19:38:45 +0800

    update board class

--- src/damath/Board.java
+++ src/damath/Board.java
@@ -10,5 +10,40 @@ package damath;
  * @author Student
  */
 public class Board {
+    //instance attributes
+    private final int boardLength = 8;
+    private final int boardWidth = 8;
+    private int player1PieceCount;
+    private int player2PieceCount;
     
+    //class constructors
+    public Board() {
+        this.player1PieceCount = 12;
+        this.player2PieceCount = 12;
+    }
+    
+    //class getters
+    
+     
+    public class Cell {
+        private int cellXPos;
+        private int cellYpos;
+        private boolean isOccupied;
+        
+        public Cell(int x, int y) {
+            this.cellXPos = x;
+            this.cellYpos = y;
+            this.isOccupied = false;
+        }
+        
+        public void changeCellState() {
+            this.isOccupied = !this.isOccupied;
+        }
+        
+        public boolean getCellState() {
+            return this.isOccupied;
+        }
+                
+    }
+            
 }
